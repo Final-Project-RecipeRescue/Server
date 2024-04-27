0a86e2347b25bdb88ec341bc269b14812ca4223e
Nissan Yamin <nissanyam1@gmail.com>
2024-04-27 16:58:23 +0300

    update

--- gas.py
+++ gas.py
@@ -1,44 +1,46 @@
 import json
 import time
 from selenium import webdriver
-from selenium.webdriver import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 
 def open_dropdown(driver):
     try:
         container_xpath = '//*[@id="select2-ingredients-list0-container"]'
         # Find the ingredients container and click on it
-        ingredients_container = WebDriverWait(driver, 20).until(
+        ingredients_container = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, container_xpath))
         )
         ingredients_container.click()
-        time.sleep(1)
+
+        # Wait for the dropdown to appear
+        id = "select2-ingredients-list0-results"
+        dropdown_list = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, id))
+        )
+
         # Return the dropdown element
-        return ingredients_container
+        return dropdown_list
 
     except Exception as e:
         print("An error occurred while opening the dropdown:", e)
         return None
 
 
-def get_categories_with_elements(driver):
+def get_categories_with_elements(dropdown):
     try:
         categories_with_elements = {}
-        # Wait for the dropdown to appear
-        id = "select2-ingredients-list0-results"
-        dropdown_list = WebDriverWait(driver, 20).until(
-            EC.visibility_of_element_located((By.ID, id))
-        )
+
         # Find all the categories with the specified class
-        categories = WebDriverWait(dropdown_list, 10).until(
+        categories = WebDriverWait(dropdown, 10).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.select2-results__option--group'))
         )
         for category in categories:
-            category_name = category.accessible_name
+            category_name = category.text.strip()
 
             options = WebDriverWait(category, 10).until(
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.select2-results__option--selectable'))
@@ -51,7 +53,7 @@ def get_categories_with_elements(driver):
         print("An error occurred while get categories:", e)
 
 
-def search_ingredient(driver, ingredient):
+def perform_search(driver, ingredient):
     try:
         search_bar_xpath = '/html/body/span[2]/span/span[1]/input'
         search_bar = WebDriverWait(driver, 10).until(
@@ -63,12 +65,7 @@ def search_ingredient(driver, ingredient):
 
         search_bar.send_keys(Keys.ENTER)
         time.sleep(1)
-    except Exception as e:
-        print(f"An error occurred while search_ingredient {ingredient}")
 
-
-def enterGram(driver):
-    try:
         gram_bar_xpath = '//*[@id="unit_quantity0"]'
         gram_bar = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, gram_bar_xpath))
@@ -78,70 +75,34 @@ def enterGram(driver):
         gram_bar.send_keys('100')
         gram_bar.click()
         time.sleep(1)
-    except Exception as e:
-        print(f"An error occurred while entering gram")
+        xpath_amount_grams_per_serving = '/html/body/div[4]/main/div/section[2]/div/div/div/div/div/div[1]/div/div[1]/div/form/div[2]/div[3]/div[2]'
+        amount_grams_per_serving = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH, xpath_amount_grams_per_serving))
+        )
 
+        amount_per_serving = amount_grams_per_serving.text
 
-def calc(driver):
-    try:
         calculate_xpath = '//*[@id="submit-button"]'
         calculate_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, calculate_xpath))
         )
         calculate_button.click()
-    except Exception as e:
-        print(f"An error occurred while entering calculation")
+        time.sleep(3)
 
-
-def get_gCo2e(driver):
-    try:
         xpath_co2_per_serving = '//*[@id="total_emissions"]'
-        gCO2e_per_serving = WebDriverWait(driver, 10).until(
+        gCO2e_per_serving = WebDriverWait(driver,10).until(
             EC.element_to_be_clickable((By.XPATH, xpath_co2_per_serving))
         )
-        return gCO2e_per_serving
-    except Exception as e:
-        print(f"An error occurred while getting gCO2e")
+        gCO2e = gCO2e_per_serving.text
 
-
-def get_description(driver):
-    try:
         web_element = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH,
-                                        '/html/body/div[4]/main/div/section[2]/div/div/div/div/div/div[1]/div/div[4]/div[1]/div/div[2]/div/div[2]/div[2]'))
+            EC.element_to_be_clickable((By.XPATH,'/html/body/div[4]/main/div/section[2]/div/div/div/div/div/div[1]/div/div[4]/div[1]/div/div[2]/div/div[2]/div[2]'))
         )
-        description_co2_per_serving = WebDriverWait(web_element, 10).until(
-            EC.element_to_be_clickable((By.ID, 'emissions_category_desc'))
+        description_co2_per_serving = WebDriverWait(web_element,10).until(
+            EC.element_to_be_clickable((By.ID,'emissions_category_desc'))
         )
-        return description_co2_per_serving
-    except Exception as e:
-        print(f"An error occurred while getting description")
+        description = description_co2_per_serving.text
 
-
-def get_amount_grams_per_serving(driver):
-    try:
-        xpath_amount_grams_per_serving = '/html/body/div[4]/main/div/section[2]/div/div/div/div/div/div[1]/div/div[1]/div/form/div[2]/div[3]/div[2]'
-        amount_grams_per_serving = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, xpath_amount_grams_per_serving))
-        )
-        return amount_grams_per_serving
-    except Exception as e:
-        print(f"An error occurred while getting amount")
-
-
-def perform_search(driver, ingredient):
-    try:
-        search_ingredient(driver, ingredient)
-        time.sleep(2)
-        enterGram(driver)
-        time.sleep(2)
-        amount_per_serving = get_amount_grams_per_serving(driver).text
-        time.sleep(2)
-        calc(driver)
-        time.sleep(5)
-        gCO2e = get_gCo2e(driver).text
-        time.sleep(2)
-        description = get_description(driver).text
         return {
             "name": ingredient,
             "amount_per_serving": amount_per_serving,
@@ -152,11 +113,12 @@ def perform_search(driver, ingredient):
     except Exception as e:
         print(f"An error occurred while performing search for '{ingredient}':", e)
 
-
 def get_all_ingredients():
-    # Initialize Selenium WebDriver (assuming you're using Chrome)
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    # Set Chrome options to run headless
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
 
     # Open the website
     driver.get("https://myemissions.green/food-carbon-footprint-calculator/")
@@ -167,27 +129,18 @@ def get_all_ingredients():
         print("dropdown opened")
         if dropdown:
             # Get categories with elements
-            categories_with_elements = get_categories_with_elements(driver)
+            categories_with_elements = get_categories_with_elements(dropdown)
             if categories_with_elements:
                 all_ingredients = {}
                 for category, elements in categories_with_elements.items():
                     category_dict = {}
                     for ingredient in elements:
-                        print(f"search for {ingredient}...")
                         ingredient_data = perform_search(driver, ingredient)
                         if ingredient_data:
-                            print(f"successfully search for {ingredient} ")
+                            print(ingredient_data)
                             category_dict[ingredient] = ingredient_data
-                        else:
-                            print(f"no {ingredient}")
-                            category_dict[ingredient] = {
-                                "name": ingredient,
-                                "amount_per_serving": '0',
-                                "gCO2e_per_100g": '0',
-                                "description": ""
-                            }
-                        open_dropdown(driver)
-                        time.sleep(2)
+                            time.sleep(2)
+                            open_dropdown(driver)
                     all_ingredients[category] = category_dict
 
                 return all_ingredients
