d6fcaf2337e3fc1b6b62e46de1c0df8575d19223
Nissan Yamin <nissanyam1@gmail.com>
2024-07-12 11:46:56 +0300

    update

--- routers/user_household.py
+++ routers/user_household.py
@@ -66,7 +66,7 @@ async def add_user(user: UserInputForAddUser):
         logger.error(f"Error creating user: {e.message}")
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e.message))
     except InvalidArgException as e:
-        logger.error(f"Error adding user: {e}")
+        logger.error(f"Error adding user: {e.message}")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e.message))
 
 
